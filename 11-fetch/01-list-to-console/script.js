7e4a40d3871968bcba32aab3069f052bc32321cf
Maud-Pixel <leleuxmaud@gmail.com>
2020-11-12 15:42:50 +0100

    exercise 11.1

--- 11-fetch/01-list-to-console/script.js
+++ 11-fetch/01-list-to-console/script.js
@@ -10,5 +10,11 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+    document.getElementById("run").addEventListener("click", ()=>
+    {
+        fetch("http://localhost:3000/heroes")
+        .then(response => response.json())
+        .then(response => console.log(JSON.stringify(response)))
+        .catch(error => console.log("Erreur : " + error ))
+    })
 })();
