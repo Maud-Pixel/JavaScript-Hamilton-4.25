b25e39da1e10ae2048e77b6d2c6bb7eb0faac429
Maud-Pixel <leleuxmaud@gmail.com>
2020-11-13 14:47:04 +0100

    exercise 11.4

--- 11-fetch/04-add/script.js
+++ 11-fetch/04-add/script.js
@@ -10,5 +10,23 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+   
+    document.getElementById("run").addEventListener("click", ()=>
+    {
+        let name = document.getElementById("hero-name").value;
+        let alterEgo = document.getElementById("hero-alter-ego").value;
+        let abilities = document.getElementById("hero-powers").value;
+        let array_abilities = []
+        array_abilities.push(abilities);
+        let result = {name, alterEgo,array_abilities}
+        let place = document.getElementById("target");
+        fetch("http://localhost:3000/heroes")
+        .then(response => response.json())
+        .then(response => {
+            response.push(result);
+            response.forEach(item => console.log(`${item.name} ${item.alterEgo} ${item.array_abilities}`)); 
+            })
+            
+    })
+    
 })();
